6b5bf6a6043371eb93a5aa427a9aad68500e0c26
Marongkun <marongkun.kwn@bytedance.com>
2023-11-05 17:51:58 +0800

    feat: all Authorize

--- src/KeJian.Core.Api/Controllers/UserController.cs
+++ src/KeJian.Core.Api/Controllers/UserController.cs
@@ -9,7 +9,7 @@ namespace KeJian.Core.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    // [Authorize]
+    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly IBaseApplication<User> _application;
